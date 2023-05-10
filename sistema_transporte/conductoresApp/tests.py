e5bfb7c6863f785b275d52464a39daf72d4ad8c9
dmlr7 <galeonp@unal.edu.co>
2023-05-10 11:28:12 -0500

    test sobre listar y crear un conductor

--- sistema_transporte/conductoresApp/tests.py
+++ sistema_transporte/conductoresApp/tests.py
@@ -1,5 +1,34 @@
 
-# Django
-from django.test import TestCase
 # rest framework
+from rest_framework import status
 from rest_framework.test import APITestCase
+# models
+from api.serializers import ConductorSerializer
+
+
+class ConductoresAPITestCase(APITestCase):
+    """Conductores APITest case"""
+
+    def setUp(self) -> None:
+        """Test case Setup"""
+        # self.token = ""
+        # self.client.credentials(self.token)
+        pass
+
+    def test_API_list_conductor(self):
+        url = "conductor//conductor"
+        request = self.client.get(url)
+        self.assertEquals(request.status_code, status.HTTP_200_OK)
+
+    def test_API_insert_conductor(self):
+        url = "conductor//conductor//"
+        conductor = ConductorSerializer(data={
+          "id": 3,
+          "identificacion": 2,
+          "nombre": "Bogota",
+          "apellido": "somewhere",
+          "telefono": 1111111111,
+          "direccion": "asdfsad asdasds3"
+        })
+        request = self.client.post(url, conductor)
+        self.assertEquals(request.status_code, status.HTTP_202_ACCEPTED)
